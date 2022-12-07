eb3af03e2abb4a48c9da142cf8d531a532b8a556
Leon Mwazange <leon@easee.com>
2022-12-07 18:43:34 +0100

    Day 1 submission
    Base it off on a priority queue.
    Started it based on a dictionary but since the requirements just needed a quick calculation, I performed the summation before adding it to the queue. 🤷🏽‍♂️
    That way, when it came to getting the bag with most calories, it was a matter of dequeuing a pre-calculated value.

---
+++ src/day1/Program.cs
@@ -0,0 +1,53 @@
+﻿const string inputFileName = "food-calories-per-elf-input.txt";
+var fileReader = File.OpenText(inputFileName);
+
+ElvesWithFood elvesWithFood = new();
+int elfId = 1;
+List<FoodItem> foodItems = new();
+
+while (true)
+{
+    var lineItem = await fileReader.ReadLineAsync();
+    if (lineItem is null)
+    {
+        break;
+    }
+
+    if (string.IsNullOrWhiteSpace(lineItem))
+    {
+        ElfBag elfBagWithFood = new ElfBag(elfId, new List<FoodItem>(foodItems));
+        elvesWithFood.Register(elfBagWithFood);
+
+        elfId++;
+        foodItems.Clear();
+        continue;
+    }
+
+    var calories = int.Parse(lineItem);
+    foodItems.Add(new FoodItem(calories));
+}
+
+(int elf, int totalCalories) = elvesWithFood.GetElfWithMostCalories();
+Console.WriteLine($"Elf #{elf} has bag with containing {totalCalories} total calories");
+
+internal record FoodItem(int Calories);
+internal record ElfBag(int ElfId, List<FoodItem> FoodItems)
+{
+    public int TotalCaloriesInBag => FoodItems.Sum(i => i.Calories);
+};
+
+internal class ElvesWithFood
+{
+    private readonly PriorityQueue<ElfBag, int> _bagsByCalories = new();
+
+    public void Register(ElfBag entry)
+    {
+        _bagsByCalories.Enqueue(entry, -entry.TotalCaloriesInBag);
+    }
+
+    public (int Elf, int TotalCalories) GetElfWithMostCalories()
+    {
+        ElfBag bagWithFood = _bagsByCalories.Dequeue();
+        return (bagWithFood.ElfId, bagWithFood.TotalCaloriesInBag);
+    }
+}
